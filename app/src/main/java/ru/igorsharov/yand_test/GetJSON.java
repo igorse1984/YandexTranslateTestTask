ccb8a515280f4300abf4a763b5ad3b566b561c81
Igor Sharoff <igor_se@mail.ru>
2017-04-03 14:04:08 +0300

    реализовано взаимодействие запроса с EditText

--- app/src/main/java/ru/igorsharov/yand_test/GetJSON.java
+++ app/src/main/java/ru/igorsharov/yand_test/GetJSON.java
@@ -18,6 +18,7 @@ public class GetJSON {
     private static final String LOG_TAG = "GetJSON";
     private static final String API_KEY = "trnsl.1.1.20170325T140225Z.5fb87348c9fc5b7a.56c642ead6f88545a2539c4e67fad415f8d7d87b";
 
+    // вспомогательный метод, HTTP провайдер
     private String getJSONString(String url) throws IOException {
         OkHttpClient client = new OkHttpClient();
         Request request = new Request.Builder()
@@ -31,21 +32,25 @@ public class GetJSON {
         return result;
     }
 
-    public void fetchItems() {
+    // основной метод класса, делает запрос на сервер для перевода введеного текста
+    public void fetchItems(String str) {
         try {
             String url = Uri.parse("https://translate.yandex.net/api/v1.5/tr.json/translate?")
                     .buildUpon()
                     .appendQueryParameter("key", API_KEY)
-                    .appendQueryParameter("text", "hello world")
-                    .appendQueryParameter("lang", "en-ru")
+                    .appendQueryParameter("text", str)
+                    .appendQueryParameter("lang", "ru-en")
                     .build().toString();
             String jsonString = getJSONString(url);
-//            JSONObject jsonBody = new JSONObject(jsonString);
+            JSONObject jsonBody = new JSONObject(jsonString);
+
+            // тестируем вывод результата
             System.out.println("JSON answer: " + jsonString);
+
         } catch (IOException ioe) {
             Log.e(LOG_TAG, "ОШИБКА ЗАГРУЗКИ ДАННЫХ", ioe);
         }
-//        catch (JSONException joe) {
-//        }
+        catch (JSONException joe) {
+        }
     }
 }
--- app/src/main/java/ru/igorsharov/yand_test/MainActivity.java
+++ app/src/main/java/ru/igorsharov/yand_test/MainActivity.java
@@ -1,9 +1,12 @@
 package ru.igorsharov.yand_test;
 
 import android.os.AsyncTask;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Toast;
+import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,19 +14,39 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        new StartParsing().execute();
+        final EditText et = (EditText) findViewById(R.id.editText);
+        final TextView tv = (TextView) findViewById(R.id.textView);
+        et.setOnKeyListener(new View.OnKeyListener()
+                            {
+                                @Override
+                                public boolean onKey(View v, int keyCode, KeyEvent event)
+                                {
+                                    if(event.getAction() == KeyEvent.ACTION_DOWN &&
+                                            (keyCode == KeyEvent.KEYCODE_ENTER))
+                                    {
+                                        // сохраняем текст, введенный до нажатия Enter в переменную
+                                        String str = et.getText().toString();
+                                        // отправляем полученную строку в обработку
+                                        new StartParsing().execute(str);
+                                        tv.setText(str);
+                                        return true;
+                                    }
+                                    return false;
+                                }
+                            }
+        );
+
+
     }
 
 
-    private class StartParsing extends AsyncTask<Void, Void, Void> {
+    private class StartParsing extends AsyncTask<String, Void, Void> {
 
         @Override
-        protected Void doInBackground(Void... params) {
-            new GetJSON().fetchItems();
+        protected Void doInBackground(String... params) {
+            new GetJSON().fetchItems(params[0]);
             return null;
         }
 
-        // test_hello
-        // test-test
     }
 }
